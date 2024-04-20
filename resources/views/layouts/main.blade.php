e0ade8173584327d3fa010deb5dfb4d0d1784156
giansoniaputra <giansonia555@gmail.com>
2024-04-20 11:01:26 +0700

    dksakdsa

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -165,7 +165,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>COPYRIGHT FEBRIAN IDZNI FAZARI 2024</span>
                     </div>
                 </div>
             </footer>
